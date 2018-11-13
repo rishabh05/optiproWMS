9da1fdd0d154f6968db6cad519a77982a99ce2bd
User1 <arjun@eamandloi@eworkplaceapps.com>
2018-11-13 12:25:22 +0530

    structure changes

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -2,12 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AccountRoutingModule } from './account-routing.module';
-import { SignupComponent } from './signup/signup.component';
 import { SigninComponent } from './signin/signin.component';
-import { SetPasswordComponent } from './set-password/set-password.component';
-import { ResetPasswordComponent } from './reset-password/reset-password.component';
-
-
 
 import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 import { HttpClientModule } from '@angular/common/http';
@@ -34,6 +29,6 @@ import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
     FormsModule,
     TooltipModule.forRoot(),
   ],
-  declarations: [SignupComponent, SigninComponent, SetPasswordComponent, ResetPasswordComponent]
+  declarations: [SigninComponent]
 })
 export class AccountModule { }
