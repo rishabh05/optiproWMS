e6d2fcc5346e522b7ba25273856e1aac32bd8bc0
User1 <arjun@eamandloi@eworkplaceapps.com>
2018-11-14 19:48:31 +0530

    vendor

--- src/app/inbound/inbound.module.ts
+++ src/app/inbound/inbound.module.ts
@@ -5,11 +5,24 @@ import { InboundRoutingModule } from './inbound-routing.module';
 import { VenderSelectionComponent } from '../inbound/vender-selection/vender-selection.component';
 import { POListComponent } from './polist/polist.component';
 import { GRPOCalculationComponent } from './grpocalculation/grpocalculation.component';
+import { GridModule, ExcelModule } from '@progress/kendo-angular-grid';
+import { FormsModule } from '@angular/forms';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { FileDropModule } from 'ngx-file-drop';
 
 @NgModule({
   imports: [
     CommonModule,
-    InboundRoutingModule
+    InboundRoutingModule,
+    GridModule,
+    ExcelModule,
+    FormsModule,
+    AngularSvgIconModule,
+    PerfectScrollbarModule,
+    DropDownsModule,
+    FileDropModule
   ],
   declarations: [VenderSelectionComponent, POListComponent, GRPOCalculationComponent],
   exports:[VenderSelectionComponent]
